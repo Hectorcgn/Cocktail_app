1e12a5acf85eb3d62b2f7a3a171cbab95e7e4f2c
Hectorcgn <124736463+Hectorcgn@users.noreply.github.com>
2023-08-15 17:00:22 +0200

    pushdata

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.scss'
+import Footer from './components/shared/Footer/Footer'
+import Main from './components/pages/Main/Main'
 
 function App() {
 
 
   return (
     <>
-      
+    
+    <Routes>
+      <Route path='/' element={<Main/>}/>
+    </Routes>
+
+      <Footer/>
     </>
   )
 }
