f49ec6d783093dc4107e458e4d279efa143f0ad8
Hectorcgn <124736463+Hectorcgn@users.noreply.github.com>
2023-08-16 15:30:40 +0200

    change drink list style

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Navbar.scss'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
     <nav>
+      <Link to={'/'}>
       <h2>DRINKS&CHILL</h2>
+      </Link>
       <p>MENU</p>
     </nav>
   )
