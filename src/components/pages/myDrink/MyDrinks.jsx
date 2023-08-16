a79b1e2f9d04ad4bf25ec0c464770dcf624c9a63
Your Name <ali3ajob@gmail.com>
2023-08-16 17:13:44 +0200

    ali-16-4

---
+++ src/components/pages/myDrink/MyDrinks.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import DrinkItem from '../../shared/DrinkItem/DrinkItem'
+import { v4 as uuidv4 } from 'uuid';
+import { AutoGrid } from '../../shared/AutoGrid/AutoGrid';
+
+function MyDrinks() {
+  const myDrinks = localStorage.getItem('myDrink').map(item => item.id = uuidv4())
+  
+  return (
+    <ul className="drinklist">
+      <AutoGrid>
+        {myDrinks.map(myDrink => <DrinkItem 
+          key={myDrink.id}
+          id={myDrink.id}
+          name={myDrink.strDrink}
+          img={myDrink.strDrinkThumb}
+          />)}
+      </AutoGrid>
+    </ul>
+  )
+}
+
+export default MyDrinks
