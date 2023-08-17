c4fe461840f1b35009be20d6668dd622e39ba074
Your Name <ali3ajob@gmail.com>
2023-08-17 15:38:01 +0200

    ali-last-update

--- src/components/shared/DrinkItem/DrinkItem.jsx
+++ src/components/shared/DrinkItem/DrinkItem.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DrinkItem.scss";
 import { Link } from "react-router-dom";
 
 function DrinkItem(props) {
+  const [imageUrl, setImageUrl] = useState(props.img)
+
+  const imageAlternative = () => {
+    setImageUrl('https://cdn.pixabay.com/photo/2021/02/02/08/38/broken-5973122_1280.jpg')
+  }
+
   return (
     <Link to={`/drinklist/details/${props.id}`}>
       <li>
         <h3>{props.name}</h3>
-        <img src={props.img} alt="coctail.img" />
+        <img src={imageUrl} onError={imageAlternative}/>
       </li>
     </Link>
   );
